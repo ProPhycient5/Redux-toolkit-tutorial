d7f6b3349f0c145a0aa1ee37448f3f61340f4945
sawan <dsawan.office8@gmail.com>
2023-11-24 23:56:52 +0530

    added code for action creator

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Redux index file")
+const CAKE_ORDERED = "CAKE_ORDERED"; //<-------This is actually constant string
+
+// {
+//     type: CAKE_ORDERED,   //<-------This is actually an action
+//     quantity: 1
+// }
+
+//action creator --> The func that returns the action
+function orderCake() {
+  return {
+    type: CAKE_ORDERED,
+    quantity: 1,
+  };
+}
