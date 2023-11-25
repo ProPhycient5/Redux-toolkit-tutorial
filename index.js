d252ff407c93595269c58be7be2b7c5b0c2fec46
sawan <dsawan.office8@gmail.com>
2023-11-25 18:46:10 +0530

    added code for restock cake and store

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+const redux = require("redux");
+const createStore = redux.createStore();
+
 const CAKE_ORDERED = "CAKE_ORDERED"; //<-------This is actually constant string
+const CAKE_RESTOCKED = "CAKE_RESTOCKED";
 
 // {
 //     type: CAKE_ORDERED,   //<-------This is actually an action
@@ -9,7 +13,14 @@ const CAKE_ORDERED = "CAKE_ORDERED"; //<-------This is actually constant string
 function orderCake() {
   return {
     type: CAKE_ORDERED,
-    quantity: 1,
+    payload: 1,
+  };
+}
+
+function restockCake(amount = 1) {
+  return {
+    type: CAKE_RESTOCKED,
+    payload: amount,
   };
 }
 
@@ -22,9 +33,29 @@ const reducer = (state = initialState, action) => {
   switch (action.type) {
     case CAKE_ORDERED:
       return {
+        ...state,
         totalCake: state.totalCake - 1,
+      };
+    case CAKE_RESTOCKED:
+      return {
+        ...state,
+        totalCake: state.totalCake + action.payload,
       };
     default:
       return state;
   }
 };
+
+const store = createStore(reducer); //store holding the state of an app.
+console.log("initial_state", store.getState());
+
+const unsubscribe = store.subscribe(() =>
+  console.log("update_state", store.getState())
+); //need to study about the subscribe method
+
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch(restockCake(4));
+
+unsubscribe();
